0db656da12cff2f0e6317b068ab797a56128b3a4
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-08-16 19:23:03 -0400

    Make quote pattern more consistent

--- update.ps1
+++ update.ps1
@@ -3,24 +3,24 @@ Import-Module AU
 function global:au_BeforeUpdate($Package) {
     $Latest.Checksum64 = Get-RemoteChecksum -Url $Latest.Url64
 
-    Set-DescriptionFromReadme -Package $Package -ReadmePath ".\DESCRIPTION.md"
+    Set-DescriptionFromReadme -Package $Package -ReadmePath '.\DESCRIPTION.md'
 }
 
 function global:au_SearchReplace {
     @{
         "$($Latest.PackageName).nuspec" = @{
-            "<packageSourceUrl>[^<]*</packageSourceUrl>" = "<packageSourceUrl>https://github.com/brogers5/chocolatey-package-$($Latest.PackageName)/tree/v$($Latest.Version)</packageSourceUrl>"
+            '<packageSourceUrl>[^<]*</packageSourceUrl>' = "<packageSourceUrl>https://github.com/brogers5/chocolatey-package-$($Latest.PackageName)/tree/v$($Latest.Version)</packageSourceUrl>"
             '(\<copyright\>).*?(\<\/copyright\>)' = "`${1}© Meta $(Get-Date -Format yyyy)`$2"
         }
-        "tools\chocolateyinstall.ps1" = @{
-            "(^[$]?\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        'tools\chocolateyinstall.ps1' = @{
+            '(^[$]?\s*checksum64\s*=\s*)(''.*'')' = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
 function global:au_GetLatest {
     $uri = 'https://www.facebook.com/messenger/desktop/zeratul/update.xml?target=zeratul&platform=win'
-    $userAgent = "Update checker of Chocolatey Community Package 'messenger'"
+    $userAgent = 'Update checker of Chocolatey Community Package ''messenger'''
     $page = Invoke-WebRequest -Uri $uri -UserAgent $userAgent -UseBasicParsing
     $xmlDocument = [xml] $page.Content
     $releaseItem = $xmlDocument.rss.channel.item
