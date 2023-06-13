8a38cc73ac953d70f5db660310a1c711b9ea7ac1
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-06-13 01:06:10 -0400

    Reformat scripts

--- tools/chocolateybeforemodify.ps1
+++ tools/chocolateybeforemodify.ps1
@@ -3,23 +3,20 @@
 $processName = 'Messenger'
 $process = Get-Process -Name $processName -ErrorAction SilentlyContinue
 
-if ($process)
-{
+if ($process) {
     Write-Warning "$processName is currently running, stopping it to prevent upgrade/uninstall from blocking..."
     Stop-Process -InputObject $process -ErrorAction SilentlyContinue
 
     Start-Sleep -Seconds 3
 
     $process = Get-Process -Name $processName -ErrorAction SilentlyContinue
-    if ($process)
-    {
+    if ($process) {
         Write-Warning "$processName is still running despite stop request, force stopping it..."
         Stop-Process -InputObject $process -Force -ErrorAction SilentlyContinue
     }
 
     Write-Warning "If upgrading, $processName may need to be manually restarted upon completion"
 }
-else
-{
+else {
     Write-Debug "No running $processName process instances were found"
 }
