e4292601c8ac858bb92c84641112fe30beb8b49f
brogers5 <6869577+brogers5@users.noreply.github.com>
2024-07-05 11:23:44 -0400

    Update to 215.2.619297709

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $installArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   silentArgs     = '/S'
-  url64bit       = 'https://www.facebook.com/zeratul/desktop/update/617581566.exe'
-  checksum64     = '8a196bd07196126249f85c14492abb79b9ab51833ddb5c4173f57bbd302d574c'
+  url64bit       = 'https://www.facebook.com/zeratul/desktop/update/619297709.exe'
+  checksum64     = '83e0d551cb1b5fc106bb5cce980888137e234a2c5bb0d7e084d9e15b9714e1d8'
   checksumType64 = 'sha256'
   validExitCodes = @(0)
 }
